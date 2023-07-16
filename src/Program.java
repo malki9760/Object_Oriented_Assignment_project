fd9c7f5d2acdfb88f5d3fee5d18de2696f2a1020
malki9760 <0504109760m@gmail.com>
2023-07-17 00:27:35 +0300

    changes in the main function

--- src/Program.java
+++ src/Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main(String[] args) {
-        Animal[] animals = { new Frog(false, false, 0), new Cat(true, true, 0), new Dog(true, true, 1)};
+        Animal[] animals = { new Dog(true, true, 1), new Frog(false, false, 0), new Cat(true, true, 0)};
         for (Animal animal : animals) {
             //Check the type of the Animal.
             String typeOfAnimal = "";
@@ -12,31 +12,32 @@ public class Program {
                 typeOfAnimal = "Frog";
             };
             System.out.println("Hi i am a "+typeOfAnimal);
-            System.out.println("usually the " + typeOfAnimal + " say hello:");
+            System.out.println("usually i say hello:");
             animal.sayHello();
             String myMood = animal.getMood() == 0? "happy mood" : animal.getMood() == 1? "scare mood" : "normal mood";
             System.out.println("but now i am in a "+ myMood +" so i am saying hello by say:" );
             animal.sayHello(animal.getMood());
             System.out.println("this is some details about me:");
             if(animal.isMammals())
-                System.out.println("the " + typeOfAnimal + " have mammal");
+                System.out.println("I am a mammal");
             if(animal.isCarnivorous())
-                System.out.println("the " + typeOfAnimal + " is a carnivorous");
+                System.out.println("I am a carnivorous");
             //Check if the animal instance of ILand if so creates a direct conversion
             if (animal instanceof ILand) {
                 ILand landAnimal = (ILand) animal;
-                System.out.println("the "+typeOfAnimal+" have "+landAnimal.getNumberOfLegs()+" legs");
+                System.out.println("I am a land animal");
+                System.out.println("and i have "+landAnimal.getNumberOfLegs()+" legs");
             }
             //Check if the animal instance of IWater if so creates a direct conversion
             if (animal instanceof IWater) {
                 IWater waterAnimal = (IWater) animal;
+                System.out.println("I am a water animal");
                 if (waterAnimal.hasLaysEggs())
-                    System.out.println("the "+typeOfAnimal+" can lay eggs");
+                    System.out.println("and i can lay eggs");
                 if (waterAnimal.hasGills())
-                    System.out.println("the "+typeOfAnimal+" have gills");
+                    System.out.println("and i have gills");
             }
             System.out.println("----------------------------------------------------------------------------------------------------");
         }
-
     }
 }
